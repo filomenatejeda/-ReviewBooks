07240367a05a4bf64d01f91635586da9892ccf06
filomenatejeda <ftejeda@alumnos.uai.cl>
2024-09-29 21:20:52 -0300

    components

--- components/Footer.js
+++ components/Footer.js
@@ -6,11 +6,11 @@ const Footer = () => {
     return (
       <footer className="bg-gray-800 text-white text-center py-4">
         <p className="text-sm">
-          &copy; {new Date().getFullYear()} Tu Nombre o Nombre del Sitio. Todos los derechos reservados.
+          &copy; {new Date().getFullYear()} Nombre del Sitio. Todos los derechos reservados.
         </p>
       </footer>
     );
   };
   
-  
+
 export default Footer;
