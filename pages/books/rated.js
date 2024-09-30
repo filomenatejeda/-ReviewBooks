c57a172f3381aadb79a16479ffb44b54adf1e382
filomenatejeda <ftejeda@alumnos.uai.cl>
2024-09-30 18:53:45 -0300

    pages

--- pages/books/rated.js
+++ pages/books/rated.js
@@ -1,13 +1,16 @@
+// pages/books/rated.js
 import React, { useState, useEffect } from 'react';
-import Link from 'next/link';
 import books from '../../components/BookData';
 import Book from '../../components/Book';
 import Footer from '../../components/Footer';
 
+import NavBar from '../../components/Navbar';
+import GenreFilter from '../../components/GenreFilter';
+
 const RatedBooks = () => {
   const [ratedBooks, setRatedBooks] = useState([]);
   const [selectedGenres, setSelectedGenres] = useState([]);
-  
+
   // Lista de géneros definidos manualmente
   const genresList = [
     'Fiction',
@@ -15,7 +18,7 @@ const RatedBooks = () => {
     'Fantasy',
     'Romance',
     'Horror',
-    'Adventure '
+    'Adventure ',
   ];
 
   useEffect(() => {
@@ -27,53 +30,33 @@ const RatedBooks = () => {
   }, []);
 
   const handleGenreChange = (genre) => {
-    setSelectedGenres(prevSelected =>
+    setSelectedGenres((prevSelected) =>
       prevSelected.includes(genre)
-        ? prevSelected.filter(g => g !== genre) // Si el género ya está seleccionado, lo eliminamos
-        : [...prevSelected, genre] // Si no está, lo agregamos
+        ? prevSelected.filter((g) => g !== genre)
+        : [...prevSelected, genre]
     );
   };
 
-  const filteredBooks = ratedBooks.filter(book => {
-    if (selectedGenres.length === 0) return true; // Si no hay géneros seleccionados, mostramos todos
-    return selectedGenres.some(genre => book.genres.includes(genre)); // Filtra por géneros seleccionados
+  const filteredBooks = ratedBooks.filter((book) => {
+    if (selectedGenres.length === 0) return true;
+    return selectedGenres.some((genre) => book.genres.includes(genre));
   });
 
   return (
-    <div className="m-0 p-0 min-h-screen flex flex-col bg-gray-100"> {/* Fondo gris claro */}
-      {/* Barra de navegación */}
-      <nav className="mb-0 p-5 bg-gray-800 text-white shadow-md">
-        <Link href={`/`}>
-          <span className="text-blue-400 hover:underline cursor-pointer">Home</span>
-        </Link>
-        <span className="mx-2"> &gt; </span>
-        <span className="text-gray-300">Mis Libros Calificados</span>
-      </nav>
-    <div className='bg-yellow-50 p-10'>
-      <h1 className="text-3xl font-bold mb-8 text-center mt-8">Mis Libros Calificados</h1>
+    <div className="m-0 p-0 min-h-screen flex flex-col bg-gray-100">
+      <NavBar />
+      <div className="bg-yellow-50 p-10">
+        <h1 className="text-3xl font-bold mb-8 text-center mt-8">Mis Libros Calificados</h1>
       </div>
-      {/* Sección de filtros con fondo gris */}
-      <div className="bg-black p-4 mb-4" style={{ backgroundColor: '#dbb377' }}> {/* Fondo gris y padding */}
-        {/* Checkboxes para filtrar por género */}
-        <div className="flex flex-wrap justify-start ml-4">
-          {genresList.map((genre) => (
-            <label key={genre} className="m-2 text-black">
-              <input
-                type="checkbox"
-                checked={selectedGenres.includes(genre)}
-                onChange={() => handleGenreChange(genre)}
-                className="mr-2"
-              />
-              {genre}
-            </label>
-          ))}
-        </div>
-      </div>
-
-      <div className="flex flex-col items-center"> {/* Contenedor centralizado */}
+      <GenreFilter
+        genresList={genresList}
+        selectedGenres={selectedGenres}
+        handleGenreChange={handleGenreChange}
+      />
+      <div className="flex flex-col items-center">
         {filteredBooks.length > 0 ? (
           filteredBooks.map((book) => (
-            <div key={book.id} className="mb-6 w-500 h-150"> {/* Limitar el ancho */}
+            <div key={book.id} className="mb-6 w-500 h-150">
               <Book book={book} />
             </div>
           ))
@@ -88,3 +71,4 @@ const RatedBooks = () => {
 };
 
 export default RatedBooks;
+
